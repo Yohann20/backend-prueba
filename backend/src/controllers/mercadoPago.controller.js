97882976d12961d31aff6494eb3fa947cd5b72c1
Yohann Matus <yohann.matus2101@gmail.com>
2025-02-24 18:18:42 -0300

    fix

--- backend/src/controllers/mercadoPago.controller.js
+++ backend/src/controllers/mercadoPago.controller.js
@@ -8,131 +8,131 @@ import MercadoPagoServices from "../services/mercadoPago.services.js";
 async function crearMercadoPagoAcc(req, res) {
     try {
         const { idMicroempresa } = req.body;
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 500, "No se ha proporcionado el id de la microempresa.");
         const [newMercadoPagoAcc, error] = await MercadoPagoServices.crearMercadoPagoAcc(idMicroempresa);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 500, error);
         }
-        return respondSuccess(res, 200, newMercadoPagoAcc);
+        return respondSuccess(req, res, 200, newMercadoPagoAcc);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> crearMercadoPagoAcc");
-        return respondError(res, 400, error);
+        return respondError(req, res, 500, error);
     }
 } 
 async function getMercadoPagoAcc(req, res) {
     try {
         const { idMicroempresa } = req.params;
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 500, "No se ha proporcionado el id de la microempresa.");
         const [mercadoPagoAcc, error] = await MercadoPagoServices.getMercadoPagoAcc(idMicroempresa);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 500, error);
         }
-        return respondSuccess(res, 200, mercadoPagoAcc);
+        return respondSuccess(req, res, 200, mercadoPagoAcc);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> getMercadoPagoAcc");
-        return respondError(res, 400, error);
+        return respondError(req, res, 500, error);
     }
 } 
 async function updateMercadoPagoAcc(req, res) {
     try {
         const { idMicroempresa } = req.params; 
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 400, "No se ha proporcionado el id de la microempresa.");
         const data = req.body;
         const [mercadoPagoAcc, error] = await MercadoPagoServices.updateMercadoPagoAcc(idMicroempresa, data);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
-        return respondSuccess(res, 200, mercadoPagoAcc);
+        return respondSuccess(req, res, 200, mercadoPagoAcc);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> updateMercadoPagoAcc");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 }
 async function deleteMercadoPagoAcc(req, res) {
     try {
         const { idMicroempresa } = req.params;
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 400, "No se ha proporcionado el id de la microempresa.");
         const [mercadoPagoAcc, error] = await MercadoPagoServices.deleteMercadoPagoAcc(idMicroempresa);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
-        return respondSuccess(res, 200, mercadoPagoAcc);
+        return respondSuccess(req, res, 200, mercadoPagoAcc);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> deleteMercadoPagoAcc");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 }
 async function getMercadoPagoAccs(req, res) {
     try {
         const [mercadoPagoAccs, error] = await MercadoPagoServices.getMercadoPagoAccs();
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
-        return respondSuccess(res, 200, mercadoPagoAccs);
+        return respondSuccess(req, res, 200, mercadoPagoAccs);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> getMercadoPagoAccs");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 } 
 async function onBoarding(req, res) {
     try {
         const { code, state } = req.query;
-        if (!code || !state) return respondError(res, 400, "No se han proporcionado los datos necesarios.");
+        if (!code || !state) return respondError(req, res, 400, "No se han proporcionado los datos necesarios.");
         console.log("Controller OnBoarding Code y state: ", code, state);
         const idMicroempresa = state;
 
         const [response, error] = await MercadoPagoServices.onBoarding(code, idMicroempresa);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
         console.log("MP: Controller OnBoarding", response);
-        return respondSuccess(res, 200, response);
+        return respondSuccess(req, res, 200, response);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> onBoarding");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 } 
 async function refreshToken(req, res) {
     try {
         const { idMicroempresa } = req.params; 
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 400, "No se ha proporcionado el id de la microempresa.");
         const [mercadoPagoAcc, error] = await MercadoPagoServices.refreshToken(idMicroempresa); 
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
-        return respondSuccess(res, 200, mercadoPagoAcc);
+        return respondSuccess(req, res, 200, mercadoPagoAcc);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> refreshToken");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 }
 async function generarUrlOnBoarding(req, res) { 
     try {
         const { idMicroempresa } = req.body; 
         
-        if (!idMicroempresa) return respondError(res, 400, "No se ha proporcionado el id de la microempresa.");
+        if (!idMicroempresa) return respondError(req, res, 400, "No se ha proporcionado el id de la microempresa.");
         console.log("MP: Controller GenerarUrl", idMicroempresa);
         const [url, error] = await MercadoPagoServices.generarUrlOnBoarding(idMicroempresa);
         if (error) {
-            return respondError(res, 400, error);
+            return respondError(req, res, 400, error);
         }
         console.log("MP: Controller GenerarUrl", url);
-        return respondSuccess(res, 200, url);
+        return respondSuccess(req, res, 200, url);
     } catch (error) {
         handleError(error, "mercadoPago.controller -> generarUrlOnBoarding");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 } 
 async function crearPreferenciaServicio(req, res) {
     try {
         const { idServicio } = req.params;
-        if (!idServicio) return respondError(res, 400, "No se ha proporcionado el id del servicio.");
+        if (!idServicio) return respondError(req, res, 400, "No se ha proporcionado el id del servicio.");
         const [iniPoint, error] = await MercadoPagoServices.crearPreferenciaServicio(idServicio);
-        if (error) return respondError(res, 400, error);
-        return respondSuccess(res, 200, { urlPago: iniPoint });
+        if (error) return respondError(req, res, 400, error);
+        return respondSuccess(req, res, 200, { urlPago: iniPoint });
     } catch (error) {
         handleError(error, "mercadoPago.controller -> crearPreferenciaServicio");
-        return respondError(res, 400, error);
+        return respondError(req, res, 400, error);
     }
 }
 export default {
